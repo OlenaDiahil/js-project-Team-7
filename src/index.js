a5a144e061afea03f36558fc6f4c8d7553824eac
OlenaDiahil <122643832+OlenaDiahil@users.noreply.github.com>
2023-07-08 14:58:21 +0300

    add-js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './js/categories';
+import './js/header';
+import './js/burger-menu';
+import './js/color-schemes';
+import './js/all-categories';
+import './js/list-templates';
+import './js/support-ukraine';
+import './js/best-sellers';
+import './js/modal-window';
+import './js/shopping-list';
+import './js/pagination';
+import './js/footer';
