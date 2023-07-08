a50c1dfb867f081484dc4e990759c9b669b00877
Yaroslav2023 <yishevchenko@ukr.net>
2023-07-08 23:52:51 +0300

    all-categories-murcup-complate

--- src/js/all-categories.js
+++ src/js/all-categories.js
@@ -2,7 +2,8 @@ import { fetchCategories, fetchCategoryBooks } from "./category-api/category-api
 
 const categoryListEl = document.querySelector('.categories_list');
 const categoryBooksEl = document.querySelector('.categories-books');
-const categoryNameEl = document.querySelector('.category-name')
+const categoryNameEl = document.querySelector('.category-name');
+const bookWrapperEL = document.querySelector('.category-books-wrap');
 
 creatAllCategories();
 
@@ -27,28 +28,35 @@ function renderCategories(category) {
    
 };
 
-function clikOnCategory(event) {
+async function clikOnCategory(event) {
+
     const categoryName = event.target.name;
+    deleteCategoryBooks();
 
-    categoryBooksEl.innerHTML = "";
-
-    fetchCategoryBooks(categoryName).then(data => {
-        categoryNameEl.textContent = categoryName;
+    const response = await fetchCategoryBooks(categoryName).then(data => {
+        if (data.list_name === "") {
+            return
+        }
         console.log(data);
         renderCategoryBooks(data);
+        categoryNameEl.textContent = categoryName;
     })
         .catch(error => Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', 1000, selectError()));
 };
   
 function renderCategoryBooks(data) {
     const markup = data.map(({ book_image, author, title }) => {
-        return `<div class="category-book-wrap">
+        return `<div class="category-book-wrapper">
         <img class="category-book-img" src="${book_image}" alt="${title}" />
-        <h2>${title}</h2>
-        <p>${author}</p>
+        <h2 class="category-name-book">${title}</h2>
+        <p class="category-name-autor">${author}</p>
         </div>`
     })
     .join("");
-    categoryBooksEl.insertAdjacentHTML('beforeend', markup);
+    bookWrapperEL.insertAdjacentHTML('beforeend', markup);
+}
+
+async function deleteCategoryBooks() {
+    bookWrapperEL.innerHTML = "";
 }
 
