920a61ba5b9d4c7d256b5faf2c84cf88961dbfae
Yuliia Diudina <yuliiadiudina@gmail.com>
2023-07-09 14:13:14 +0300

    add theme js

---
+++ src/js/color-schemes.js
@@ -0,0 +1,24 @@
+const switcherToChangeTheme = document.querySelector('#switcher-button');
+
+function setDarkTheme() {
+  document.body.classList.add('dark');
+  localStorage.theme = 'dark';
+}
+
+function setLightTheme() {
+  document.body.classList.remove('dark');
+  localStorage.theme = 'light';
+}
+
+switcherToChangeTheme.addEventListener('click', () => {
+  if (document.body.classList.contains('dark')) {
+    setLightTheme();
+  } else {
+    setDarkTheme();
+  }
+});
+
+if (localStorage.theme === 'dark') {
+  document.body.classList.add('dark');
+  switcherToChangeTheme.checked = true;
+}
