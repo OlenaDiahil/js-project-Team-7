14ec17d322e243f269acdda898f2d1ca2dc7a92f
Bohdana Berynda <122972329+DanaZadernovskaya@users.noreply.github.com>
2023-07-09 15:00:49 +0200

    support-ukraine feat js, feat favicon, feat refs

---
+++ src/js/support-ukraine/support-ukraine.js
@@ -0,0 +1,45 @@
+import fonds from './fonds';
+import { Navigation } from 'swiper/modules';
+import Swiper from 'swiper';
+
+const list = document.querySelector('.support-list');
+
+const html = fonds.map(makeMarkup).join('');
+
+function makeMarkup({ url, title, img }, index) {
+  const digits = (index + 1).toString().padStart(2, '0');
+
+  return `
+  <li class="swiper-slide">
+        <div class="support-item">
+        <span class="support-index">${digits}</span>
+        <a class="support-link" href="${url}" target="_blank" rel="noopener noreferrer nofollow">
+            <img
+                srcset="${img.normal} 1x, ${img.retina} 2x"
+                src="${img.normal}" type="image/png" alt="${title}">
+        </a>
+    </li>`;
+}
+
+list.innerHTML = html;
+
+const str = fonds.map((element, index) => {
+  return '<li class="support-item"> Посилання на фонд <a class="support-link" href=""></a></li>';
+});
+
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  slidesPerView: 4,
+  rewind: true,
+  spaceBetween: 20,
+  effect: 'slide',
+  breakpoints: {
+    480: {
+      slidesPerView: 6,
+    },
+  },
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-next',
+  },
+});
